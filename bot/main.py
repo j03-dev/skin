7ba12f09ae6fc25f69e01a98df22bf0758575820
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-01-22 14:46:49 +0300

    send image from messenge to server withou saving it

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,12 @@
 import os
 import io
 import requests
-import skin_ai
 import dotenv
+import logging
 from oxapy import HttpServer, Request, Response, Router, Status, exceptions, get, post
 
 
 dotenv.load_dotenv()
-
-MODEL = skin_ai.load_model(path="assets/models/mpox_model-v3.h5")
 
 
 @get("/")
@@ -30,16 +28,19 @@ def predict(image_url: str):
     response = requests.get(image_url)
     response.raise_for_status()
     file = {"image": ("image.jpg", io.BytesIO(response.content), "image/jpeg")}
-    response = requests.post("http://locahost:8000/api/v1/predict", files=file)
+    response = requests.post("http://localhost:8000/api/v1/predict", files=file)
     response.raise_for_status()
     return response.json()
 
 
 def send_text(psid: str, text: str):
-    url = "https://graph.facebook.com/v19/me/messages"
-    params = {"access_token": os.getenv("ACCESS_TOKEN")}
+    url = "https://graph.facebook.com/v21.0/me/messages"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.getenv('ACCESS_TOKEN')}",
+    }
     payload = {"recipient": {"id": psid}, "message": {"text": text}}
-    r = requests.post(url, params=params, json=payload)
+    r = requests.post(url, headers=headers, json=payload)
     r.raise_for_status()
 
 
@@ -48,20 +49,28 @@ def webhook_core(r: Request):
     data = r.json()
     messaging = data["entry"][0]["messaging"][0]
     sender = messaging["sender"]["id"]
-    image_url = messaging["message"]["attachements"][0]["playload"]["url"]
-    prediction = predict(image_url)
-    send_text(
-        sender,
-        f"label={prediction['label']} prediction={prediction['confidence']}",
-    )
+    if message := messaging.get("message"):
+        image_url = message["attachments"][0]["payload"]["url"]
+        prediction = predict(image_url)
+        send_text(
+            sender,
+            f"label={prediction['label']} prediction={prediction['confidence']}",
+        )
     return Status.OK
+
+
+def log(r: Request, next, **kwargs):
+    logging.log(1000, f"{r.uri} {r.method}")
+    return next(r, **kwargs)
 
 
 def main():
     (
         HttpServer(("0.0.0.0", 5555))
         .attach(
-            Router("/webhook").routes(
+            Router("/webhook")
+            .middleware(log)
+            .routes(
                 [
                     webhook_verify,
                     webhook_core,
