da837599f6f4f6b758848a7c48d2076baa7fa199
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-01-23 11:23:24 +0300

    make it more comprehensible

--- bot/main.py
+++ bot/main.py
@@ -7,6 +7,8 @@ from oxapy import HttpServer, Request, Response, Router, Status, exceptions, get
 
 
 dotenv.load_dotenv()
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
 
 
 @get("/")
@@ -17,27 +19,32 @@ def webhook_verify(r: Request):
     hub_verify_token: str | None = query.get("hub.verify_token")
 
     if hub_mode and hub_challenge and hub_verify_token:
-        if hub_mode == "subscribe" and hub_verify_token == os.getenv("VERIFY_TOKEN"):
+        if hub_mode == "subscribe" and hub_verify_token == VERIFY_TOKEN:
             return Response(hub_challenge, content_type="text/plain")
     raise exceptions.UnauthorizedError(
         "Insufficient arguments provided or Token mismatch"
     )
 
 
-def predict(image_url: str):
-    response = requests.get(image_url)
-    response.raise_for_status()
-    file = {"image": ("image.jpg", io.BytesIO(response.content), "image/jpeg")}
+def service_prediction(image_bytes: bytes) -> dict[str, float]:
+    file = {"image": ("image.jpg", io.BytesIO(image_bytes), "image/jpeg")}
     response = requests.post("http://localhost:8000/api/v1/predict", files=file)
     response.raise_for_status()
     return response.json()
+
+
+def get_image_content(message: dict) -> bytes:
+    image_url = message["attachments"][0]["payload"]["url"]
+    response = requests.get(image_url)
+    response.raise_for_status()
+    return response.content
 
 
 def send_text(psid: str, text: str):
     url = "https://graph.facebook.com/v21.0/me/messages"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {os.getenv('ACCESS_TOKEN')}",
+        "Authorization": f"Bearer {PAGE_ACCESS_TOKEN}",
     }
     payload = {"recipient": {"id": psid}, "message": {"text": text}}
     r = requests.post(url, headers=headers, json=payload)
@@ -50,12 +57,12 @@ def webhook_core(r: Request):
     messaging = data["entry"][0]["messaging"][0]
     sender = messaging["sender"]["id"]
     if message := messaging.get("message"):
-        image_url = message["attachments"][0]["payload"]["url"]
-        prediction = predict(image_url)
-        send_text(
-            sender,
-            f"label={prediction['label']} confidence={prediction['confidence']}",
+        image_bytes = get_image_content(message)
+        prediction = service_prediction(image_bytes)
+        text_message = (
+            f"label={prediction['label']} confidence={prediction['confidence']}"
         )
+        send_text(sender, text_message)
     return Status.OK
 
 
